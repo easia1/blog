0fe0f985d8cfe58470e8f7e4fd951d5d67e55a88
Edric Emmanuel Sia <edric.sia@gmail.com>
2022-02-05 07:42:54 +0800

    Added logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,10 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
+
+  # #! Added logger
+  # config.logger = Logger.new(STDOUT)
+  
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
