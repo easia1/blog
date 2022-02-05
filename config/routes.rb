245ece701057955c87ac2dcf47d6de71dcef7c7f
Edric Emmanuel Sia <edric.sia@gmail.com>
2022-02-05 08:14:27 +0800

    Uncommented routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
+  devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
   resources :categories do
     resources :tasks,  only: [:index, :show, :new, :create]
   end
@@ -24,15 +24,15 @@ Rails.application.routes.draw do
   #   resources :comments
   # end
   
-  # devise_scope :user do
-  #   authenticated :user do
-  #     root :to => 'categories#home', as: :authenticated_root
-  #   end
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'categories#home', as: :authenticated_root
+    end
   
-  #   unauthenticated do
-  #     root 'devise/sessions#new', as: :unauthenticated_root
-  #   end
-  # end
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 
 
   # root "login"
